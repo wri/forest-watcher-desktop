21f5c29d2bec564433fd6da619ab573784af5530
Kinshuk Govil <kinshuk.govil@wri.org>
2026-09-16 11:03:46 -0700

    Remove Z0* countries

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,5 +1,7 @@
 import { API_VIZZUALITY_URL_V1 } from "../constants/global";
 import { BaseService } from "./baseService";
+// countries.json was generated using the GFW Data API for administrative boundaries.
+// curl -X POST --post302 -L -H 'x-api-key: xxx' https://data-api.globalforestwatch.org/dataset/gadm_administrative_boundaries/v4.1.85/query/json --header 'Content-Type: application/json' --data '{ "sql": "SELECT name_0, gid_0, gfw_bbox FROM data WHERE adm_level=%270%27 AND gid_0 not like %27Z0%%%27 ORDER BY name_0"}' -o countries.json
 import countriesData from "../assets/data/countries.json";
 
 // Transform a GFW Data API bbox tuple [minX, minY, maxX, maxY] into a
